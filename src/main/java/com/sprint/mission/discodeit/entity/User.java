3a469a02bb35b9ffb4852bbc6ccfa1cccacfadc9
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-07-11 15:15:38 +0900

    feat: add User.java

---
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.util.UUID;
+
+public class User {
+	private UUID id;
+
+	public static void main(String[] args) {
+		System.out.println(new User().toString());
+	}
+}
+
+
