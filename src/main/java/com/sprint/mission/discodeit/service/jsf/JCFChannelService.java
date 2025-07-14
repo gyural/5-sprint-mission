d8297f08ef4f2aaf51c81d999b48873e331a0aa7
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-07-14 11:11:12 +0900

    feat: 서비스 인터페이스 및 구현체 클래스 생성

---
+++ src/main/java/com/sprint/mission/discodeit/service/jsf/JCFChannelService.java
@@ -0,0 +1,11 @@
+package com.sprint.mission.discodeit.service.jsf;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sprint.mission.discodeit.entity.Message;
+
+public class JCFChannelService {
+	private static final Map<String, Message> data = new HashMap<>();
+
+}
---
+++ src/main/java/com/sprint/mission/discodeit/service/jsf/JCFUserService.java
@@ -0,0 +1,10 @@
+package com.sprint.mission.discodeit.service.jsf;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sprint.mission.discodeit.entity.User;
+
+public class JCFUserService {
+	private static final Map<String, User> data = new HashMap<>();
+}
