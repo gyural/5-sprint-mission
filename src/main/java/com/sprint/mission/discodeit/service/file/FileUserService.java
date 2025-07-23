12bc81a61d196cdf0b510850f86fb0c5ebdcfc11
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-07-23 15:04:25 +0900

    feat: 스프린트 미션 2 필요한 패키지 및 클래스 인터페이스 정리 완료

---
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
@@ -0,0 +1,44 @@
+package com.sprint.mission.discodeit.service.file;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.UserService;
+
+public class FileUserService implements UserService {
+	@Override
+	public void create(String username) {
+
+	}
+
+	@Override
+	public void delete(UUID userId) {
+
+	}
+
+	@Override
+	public void update(UUID userId, String newUsername) {
+
+	}
+
+	@Override
+	public User read(UUID userId) {
+		return null;
+	}
+
+	@Override
+	public List<User> readAll() {
+		return List.of();
+	}
+
+	@Override
+	public boolean isEmpty(UUID userId) {
+		return false;
+	}
+
+	@Override
+	public void deleteAll() {
+
+	}
+}
