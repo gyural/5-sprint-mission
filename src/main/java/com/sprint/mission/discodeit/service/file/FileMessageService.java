8cc59cb8d67625e286e74aba469c679b21d245ed
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-07-23 15:04:25 +0900

    feat: 스프린트 미션 2 필요한 패키지 및 클래스 인터페이스 정리 완료

---
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileMessageService.java
@@ -0,0 +1,50 @@
+package com.sprint.mission.discodeit.service.file;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.service.MessageService;
+
+public class FileMessageService implements MessageService {
+
+	@Override
+	public void create(String content, UUID channelId, UUID userId) {
+
+	}
+
+	@Override
+	public void delete(UUID id) {
+
+	}
+
+	@Override
+	public void deleteAll() {
+
+	}
+
+	@Override
+	public void deleteByChannelId(UUID channelId) {
+
+	}
+
+	@Override
+	public void update(UUID id, String newContent) {
+
+	}
+
+	@Override
+	public Message read(UUID id) {
+		return null;
+	}
+
+	@Override
+	public List<Message> readAll() {
+		return List.of();
+	}
+
+	@Override
+	public List<Message> readAllByChannelId(UUID channelId) {
+		return List.of();
+	}
+}
