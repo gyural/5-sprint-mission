35a7c03edc86e310442f0692af88f414c3a88c67
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-07-24 10:31:36 +0900

    feat: spring-mission-2 기본 요구사항 완료

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,10 +1,12 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.UUID;
 
 public class Message implements Serializable {
+	@Serial
 	private static final long serialVersionUID = 1L;
 
 	private final UUID id;
