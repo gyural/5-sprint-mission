27144a0db4137825617fd5815ef9ebffae2daf12
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-07-24 10:55:32 +0900

    feat: spring-mission-2 심화 요구사항 완료

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -0,0 +1,87 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+
+public class BasicMessageService implements MessageService {
+
+	private final MessageRepository messageRepository;
+	private final UserService userService;
+	private final ChannelRepository channelRepository;
+
+	public BasicMessageService(MessageRepository messageRepository, UserService userService,
+	  ChannelRepository channelRepository) {
+		this.messageRepository = messageRepository;
+		this.userService = userService;
+		this.channelRepository = channelRepository;
+	}
+
+	@Override
+	public Message create(String content, UUID channelId, UUID userId) {
+
+		if (content == null || content.isEmpty()) {
+			throw new IllegalArgumentException("Content cannot be null or empty");
+		}
+		if (channelId == null || channelRepository.isEmpty(channelId)) {
+			throw new IllegalArgumentException("Channel ID cannot be null or empty");
+		}
+		if (userId == null || userService.isEmpty(userId)) {
+			throw new IllegalArgumentException("User ID cannot be null or empty");
+		}
+
+		return messageRepository.create(content, channelId, userId);
+	}
+
+	@Override
+	public void delete(UUID id) {
+		messageRepository.delete(id);
+	}
+
+	@Override
+	public void deleteAll() {
+		messageRepository.deleteAll();
+	}
+
+	@Override
+	public void deleteByChannelId(UUID channelId) {
+		if (channelRepository.isEmpty(channelId)) {
+			throw new IllegalArgumentException("Channel ID cannot be null or empty");
+		}
+		messageRepository.deleteByChannelId(channelId);
+	}
+
+	@Override
+	public void update(UUID id, String newContent) {
+		if (newContent == null || newContent.isEmpty()) {
+			throw new IllegalArgumentException("New content cannot be null or empty");
+		}
+
+		messageRepository.update(id, newContent);
+	}
+
+	@Override
+	public Message read(UUID id) {
+		return messageRepository.find(id);
+	}
+
+	@Override
+	public List<Message> readAll() {
+		return messageRepository.findAll();
+	}
+
+	@Override
+	public List<Message> readAllByChannelId(UUID channelId) {
+		return messageRepository.findAllByChannelId(channelId);
+	}
+
+	@Override
+	public boolean isEmpty(UUID channelId) {
+		return messageRepository.isEmpty(channelId);
+	}
+}
