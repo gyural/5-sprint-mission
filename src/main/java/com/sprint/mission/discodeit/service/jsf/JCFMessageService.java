15ccf858b2faf4c0a0e3a48edd41148f0dc226fd
Lim, gyuseong <dla93190@naver.com>
2025-08-03 19:39:01 +0900

    Revert "feat: 심화사항-메시지생성시 알람 발송"
    This reverts commit 93a21a9bd0ce55a583f31772145bb0625b76d40b.

--- src/main/java/com/sprint/mission/discodeit/service/jsf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jsf/JCFMessageService.java
@@ -7,17 +7,14 @@ import java.util.UUID;
 
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.service.MessageService;
-import com.sprint.mission.discodeit.service.alarm.MessageAlarmService;
 
 public class JCFMessageService implements MessageService {
 	public final Map<UUID, Message> data;
 	private final JCFUserService userService;
-	private final MessageAlarmService messageAlarmService;
 
-	public JCFMessageService(JCFUserService userService, MessageAlarmService messageAlarmService) {
+	public JCFMessageService(JCFUserService userService) {
 		data = new HashMap<>();
 		this.userService = userService;
-		this.messageAlarmService = messageAlarmService;
 	}
 
 	@Override
@@ -31,8 +28,6 @@ public class JCFMessageService implements MessageService {
 		}
 		Message newMessage = new Message(content, userId, channelId, userService.read(userId).getUsername());
 		data.put(newMessage.getId(), newMessage);
-
-		messageAlarmService.sendMessageAlarm(newMessage);
 	}
 
 	@Override
