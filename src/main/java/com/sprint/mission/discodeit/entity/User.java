b9a0cd75ab88ff038f0ca972444c8e4792c641b3
Lim, gyuseong <dla93190@naver.com>
2025-08-03 19:39:01 +0900

    Revert "feat: 도메인 모델링"
    This reverts commit 641e19e151c3009171639585cd3512f36bf716ea.

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,26 +1,12 @@
 package com.sprint.mission.discodeit.entity;
 
-public class User extends Common {
-	private String username;
+import java.util.UUID;
 
-	public User(String username) {
-		this.username = username;
-	}
+public class User {
+	private UUID id;
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	@Override
-	public String toString() {
-		return super.toString()+"\n" +
-		  "User{" +
-		  "username='" + username + '\'' +
-		  '}';
+	public static void main(String[] args) {
+		System.out.println(new User().toString());
 	}
 }
 
