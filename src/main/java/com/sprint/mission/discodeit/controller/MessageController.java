9d426d8f0f8e2196eb68368db52369a38415f42b
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-08-20 16:46:13 +0900

    refactor: consumse 인자 객체로 변경

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -1,7 +1,7 @@
 package com.sprint.mission.discodeit.controller;
 
 import static com.sprint.mission.discodeit.service.basic.BasicMessageService.*;
-import static org.springframework.web.bind.annotation.RequestMethod.*;
+import static org.springframework.http.MediaType.*;
 
 import java.io.IOException;
 import java.net.URI;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,7 +45,7 @@ public class MessageController {
 
 	private final MessageService messageService;
 
-	@RequestMapping(value = "", method = POST, consumes = "multipart/form-data")
+	@PostMapping(consumes = MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<CreateMessageResponse> createMessage(
 	  @RequestPart MessageCreateRequest messageCreateRequest,
 	  @RequestPart(required = false) List<MultipartFile> attachments) {
